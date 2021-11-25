91872508795aecd9a4bd8640b355711defb62271
Kateryna Grytsko <katia.grytsko@student.uj.edu.pl>
2021-11-25 23:14:05 +0100

    Unit tests added

---
+++ ApplicationTests/UnitTest1.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+
+namespace ApplicationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test5()
+        {
+            Assert.Pass();
+        }
+    }
+}
